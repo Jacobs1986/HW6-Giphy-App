4b47bfde6dc74397323da5e23fa381ef7565ccc5
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-12 11:14:35 -0700

    We've got Buttons!
    I created the function for generating buttons. I decided to have the forEach loop outside of the functions structure. I called it and it worked!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,14 @@
 //I need a function that will generate buttons from an array.
-function buttonGenerate(array) {
+function buttonGenerate(item) {
     // first I need to select the button element in jquery
-    // A loop will need to be created that reads through the entire array.
+    button = $("<button>");
     // Inside the loop an attr of data-* will need to be made so that each button can be distinguished from the other
+    button.attr("data-sport", item).text(item);
     // The button then needs to appear in the #button-area div
+    $("#button-area").append(button);
 }
 
 //Array containing names for the buttons.
 let buttonArray = ["Basketball", "Baseball", "Football"];
+
+buttonArray.forEach(buttonGenerate);
