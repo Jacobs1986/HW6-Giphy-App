2a77527a294e47677e21b5a360a3825eff8adaf6
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-12 11:06:08 -0700

    Scaffolded buttonGenerate
    Created the scaffold for the function buttonGenerate. I need it to read from the array and generate a button using a foreach loop and then for it to appear in the button-area div.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,10 @@
 //I need a function that will generate buttons from an array.
+function buttonGenerate(array) {
+    // first I need to select the button element in jquery
+    // A loop will need to be created that reads through the entire array.
+    // Inside the loop an attr of data-* will need to be made so that each button can be distinguished from the other
+    // The button then needs to appear in the #button-area div
+}
 
 //Array containing names for the buttons.
 let buttonArray = ["Basketball", "Baseball", "Football"];
