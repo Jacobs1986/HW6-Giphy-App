63890b796e8b174330a2cf0ad6f202ead935a139
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-12 11:53:56 -0700

    Grab title
    Created a simple grab of the title from the array and to display it in the console just to make sure that it worked well.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,7 +37,11 @@ $("button").on("click", function () {
             // I need to grab information from the different arrays, I'll use forEach again
             results.forEach(array => {
                 // I need to create a div for each GIF, I'll call it sportCard
+                let sportCard = $("<div>");
                 // Inside the array I need to grab a title, rating and the gif itself
+                // Get the title
+                let title = array.title;
+                console.log(title);
                 // Then it will need to be appended to the #gif-area div
             })
         })
